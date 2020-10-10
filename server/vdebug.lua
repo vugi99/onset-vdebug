9965f975a12746d81d77b5d21d2e7fcd4f2f0353
vugi99 <voltaism.mail@gmail.com>
2020-10-10 13:21:26 +0200

    remove debug code

--- server/vdebug.lua
+++ server/vdebug.lua
@@ -23,12 +23,12 @@ for i, v in ipairs(Events) do
    end)
 end
 
-AddEvent("OnPackageStart", function()
+--[[AddEvent("OnPackageStart", function()
    CreateNPC(126000.000000, 80246.000000, 1600.000000, -90)
    CreateObject(1, 126200, 80400, 1600)
    CreatePickup(1, 126200, 80600, 1600)
    CreateText3D("test", 16, 126000, 80800, 1600, 0, 0, 0)
-end)
+end)]]--
 
 AddEvent("OnPackageStart", function()
    print("vDebug Loaded")
